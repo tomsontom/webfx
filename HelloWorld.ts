2b2522dfbe6d153dd8e13e181164b059ca68c206
Gerrit Grunwald <han.solo@mac.com>
2017-12-08 18:20:58 +0100

    Added a Canvas node

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -3,6 +3,7 @@
 /// <reference path="./scene/text/Text.ts" />
 /// <reference path="./scene/control/Button.ts" />
 
+
 function run() {
     var svg = document.getElementById("sample");
     var button = new scene.control.Button("Hello SVG!");
@@ -10,4 +11,13 @@ function run() {
     button.resize(200,30);
     button.layoutChildren();
     console.log(svg);
+
+    var canvas = new scene.canvas.Canvas(200, 200);
+    var svg1 = document.getElementById("svg");
+    svg1.appendChild((canvas.getNgNode() as svgscene.canvas.NSVGCanvas).getDom());
+
+    var ctx = canvas.getGraphicsContext2D();
+    ctx.fillStyle = 'red';
+    ctx.fillRect(10, 10, 50, 50);
+
 }
