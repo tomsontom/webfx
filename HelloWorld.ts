bf56190e5b262a53f7b867e7563cb2a1e6efcb94
Gerrit Grunwald <han.solo@mac.com>
2017-12-08 18:39:43 +0100

    cosmetics

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -16,8 +16,13 @@ function run() {
     var svg1 = document.getElementById("svg");
     svg1.appendChild((canvas.getNgNode() as svgscene.canvas.NSVGCanvas).getDom());
 
+    canvas.resize(400, 400);
     var ctx = canvas.getGraphicsContext2D();
+    ctx.clearRect(0, 0, 400, 400);
     ctx.fillStyle = 'red';
-    ctx.fillRect(10, 10, 50, 50);
-
+    ctx.fillRect(10, 10, 100, 25);
+    ctx.fillRect(10, 60, 100, 25);
+    ctx.strokeStyle = 'black';
+    ctx.font = '48px sans-serf';
+    ctx.strokeText('WebFX is fun', 125, 75);
 }
