73c357f227161b215697f4fcc1f9011df0d556e4
Gerrit Grunwald <han.solo@mac.com>
2017-12-11 22:53:05 +0100

    Addded more methods to Path2D

--- geom/Path2D.ts
+++ geom/Path2D.ts
@@ -41,7 +41,7 @@ namespace geom {
 
         next() : void {
             let type = this.path.pointTypes[this.typeIdx++];
-            this.pointIdx += curvecoords[type];
+            this.pointIdx += Path2D.curvecoords[type];
         }
     }
 
@@ -55,20 +55,33 @@ namespace geom {
 
         currentSegment(coords : number[]) : number {
             let type      = this.path.pointTypes[this.typeIdx];
-            let numCoords = curvecoords[type];
+            let numCoords = Path2D.curvecoords[type];
             if (numCoords > 0) {
-                System.arraycopy(floatCoords, pointIdx, coords, 0, numCoords);
+                // alternatively copy array
+                //coords = this.floatCoords.slice(0);
+                for (let i : number = 0; i < numCoords; i++) {
+                    coords[i] = this.floatCoords[this.pointIdx + i];
+                }
             }
             return type;
         }
+    }
+
+    export class TxIterator extends geom.Iterator {
+        floatCoords : number[];
+        transform   : geom.transform.BaseTransform;
+
+        constructor(p2df : Path2D, tx : geom.transform.BaseTransform) {
+            super(p2df);
+            this.floatCoords = p2df.floatCoords;
+            this.transform   = tx;
+        }
 
         currentSegment(coords : number[]) : number {
-            let type = this.path.pointTypes[this.typeIdx];
-            let numCoords = curvecoords[type];
+            let type      : number = this.path.pointTypes[this.typeIdx];
+            let numCoords : number = Path2D.curvecoords[type];
             if (numCoords > 0) {
-                for (int i = 0; i < numCoords; i++) {
-                    coords[i] = floatCoords[pointIdx + i];
-                }
+                this.transform.transform(this.floatCoords, this.pointIdx, coords, 0, numCoords / 2);
             }
             return type;
         }
@@ -87,10 +100,10 @@ namespace geom {
 
         static readonly curvecoords: number[] = [2, 2, 4, 6, 0];
 
-        pointTypes: number[];
-        numTypes: number;
-        numCoords: number;
-        windingRule: number;
+        pointTypes  : number[];
+        numTypes    : number;
+        numCoords   : number;
+        windingRule : number;
 
         static readonly INIT_SIZE: number  = 20;
         static readonly EXPAND_MAX: number = 500;
@@ -358,9 +371,11 @@ namespace geom {
             return crossings;
         }
 
-
-        append(pi : geom.PathIterator, connect : boolean) : void {
-            let coords :number[] = [];
+        append(pi : geom.PathIterator | geom.Shape, connect : boolean) : void {
+            if (pi instanceof geom.Shape) {
+                pi = pi.getPathIterator(null);
+            }
+            let coords: number[] = [];
             while (!pi.isDone()) {
                 switch (pi.currentSegment(coords)) {
                     case geom.SegmentType.MOVE_TO:
@@ -369,8 +384,8 @@ namespace geom {
                             break;
                         }
                         if (this.pointTypes[this.numTypes - 1] != geom.SegmentType.CLOSE &&
-                            this.floatCoords[this.numCoords-2] == coords[0] &&
-                            this.floatCoords[this.numCoords-1] == coords[1]) {
+                            this.floatCoords[this.numCoords - 2] == coords[0] &&
+                            this.floatCoords[this.numCoords - 1] == coords[1]) {
                             break;
                         }
                     case geom.SegmentType.LINE_TO:
@@ -391,7 +406,7 @@ namespace geom {
             }
         }
 
-        transform(tx : geom.BaseTransform) : void {
+        transform(tx : geom.transform.BaseTransform) : void {
             if (this.numCoords == 0) return;
             this.needRoom(false, 6);
             this.floatCoords[this.numCoords + 0] = this.moveX;
@@ -439,14 +454,6 @@ namespace geom {
             return this.floatCoords;
         }
 
-        getPathIterator(tx : geom.BaseTransform) : geom.PathIterator{
-            if (tx == null) {
-                return new CopyIterator(this);
-            } else {
-                return new TxIterator(this, tx);
-            }
-        }
-
         pathDone(): void {
             if (this.numTypes == 0 || this.pointTypes[this.numTypes - 1] != geom.SegmentType.CLOSE) {
                 this.needRoom(true, 0);
@@ -454,6 +461,166 @@ namespace geom {
                 this.prevX = this.currX = this.moveX;
                 this.prevY = this.currY = this.moveY;
             }
+        }
+
+        getWindingRule() : number{
+            return this.windingRule;
+        }
+        setWindingRule(rule : number) : void {
+            if (rule != Path2D.WIND_EVEN_ODD && rule != Path2D.WIND_NON_ZERO) {
+                throw new SyntaxError("winding rule must be WIND_EVEN_ODD or WIND_NON_ZERO");
+            }
+            this.windingRule = rule;
+        }
+
+        getCurrentPoint() : geom.Point2D {
+            if (this.numTypes < 1) {
+                return null;
+            }
+            return new geom.Point2D(this.currX, this.currY);
+        }
+
+        getCurrentX() : number{
+            if (this.numTypes < 1) {
+                throw new SyntaxError("no current point in empty path");
+            }
+            return this.currX;
+        }
+
+        getCurrentY() : number{
+            if (this.numTypes < 1) {
+                throw new SyntaxError("no current point in empty path");
+            }
+            return this.currY;
+        }
+
+        reset() : void{
+            this.numTypes = this.numCoords = 0;
+            this.moveX = this.moveY = this.prevX = this.prevY = this.currX = this.currY = 0;
+        }
+
+        createTransformedShape(tx : geom.transform.BaseTransform) : geom.Shape{
+            return new geom.Path2D(this, tx);
+        }
+
+        copy() : geom.Path2D {
+            return new Path2D(this);
+        }
+
+        static contains(pi : PathIterator, x? : number, y? : number, p? : geom.Point2D) : boolean {
+            if (p !== undefined && p instanceof geom.Point2D) {
+                x = p.x;
+                y = p.y;
+            }
+
+            if (x * 0 + y * 0 == 0) {
+                let mask: number  = (pi.getWindingRule() == Path2D.WIND_NON_ZERO ? -1 : 1);
+                let cross: number = geom.Shape.pointCrossingsForPath(pi, x, y);
+                return ((cross & mask) != 0);
+            } else {
+                return false;
+            }
+        }
+        static contains(pi : geom.PathIterator, x : number, y : number, w : number, h : number) : boolean {
+            if (Number.isNaN(x + w) || Number.isNaN(y + h)) {
+                return false;
+            }
+            if (w <= 0 || h <= 0) {
+                return false;
+            }
+            let mask      : number = (pi.getWindingRule() == Path2D.WIND_NON_ZERO ? -1 : 2);
+            let crossings : number = geom.Shape.rectCrossingsForPath(pi, x, y, x + w, y + h);
+            return (crossings != geom.Shape.RECT_INTERSECTS && (crossings & mask) != 0);
+        }
+
+        contains(x? : number, y? : number, p? : geom.Point2D) : boolean {
+            if (p !== undefined && p instanceof geom.Point2D) {
+                x = p.x;
+                y = p.y;
+            }
+            if (x * 0 + y * 0 == 0) {
+                if (this.numTypes < 2) {
+                    return false;
+                }
+                let mask : number = (this.windingRule == Path2D.WIND_NON_ZERO ? -1 : 1);
+                return ((this.pointCrossings(x, y) & mask) != 0);
+            } else {
+                return false;
+            }
+        }
+
+        contains(x : number, y : number, w : number, h : number) : boolean {
+            if (Number.isNaN(x + w) || Number.isNaN(y + h)) {
+                return false;
+            }
+            if (w <= 0 || h <= 0) {
+                return false;
+            }
+            let mask      : number = (this.windingRule == Path2D.WIND_NON_ZERO ? -1 : 2);
+            let crossings : number = this.rectCrossings(x, y, x + w, y + h);
+            return (crossings != geom.Shape.RECT_INTERSECTS && (crossings & mask) != 0);
+        }
+
+        static intersects(pi : geom.PathIterator, x : number, y : number, w : number, h : number) : boolean {
+            if (Number.isNaN(x + w) || Number.isNaN(y + h)) {
+                return false;
+            }
+            if (w <= 0 || h <= 0) {
+                return false;
+            }
+            let mask      : number = (pi.getWindingRule() == Path2D.WIND_NON_ZERO ? -1 : 2);
+            let crossings : number = geom.Shape.rectCrossingsForPath(pi, x, y, x + w, y + h);
+            return (crossings == geom.Shape.RECT_INTERSECTS || (crossings & mask) != 0);
+        }
+
+        intersects(x : number, y : number, w : number, h : number) : boolean {
+            if (Number.isNaN(x + w) || Number.isNaN(y + h)) {
+                return false;
+            }
+            if (w <= 0 || h <= 0) {
+                return false;
+            }
+            let mask      : number = (this.windingRule == Path2D.WIND_NON_ZERO ? -1 : 2);
+            let crossings : number = this.rectCrossings(x, y, x + w, y + h);
+            return (crossings == geom.Shape.RECT_INTERSECTS || (crossings & mask) != 0);
+        }
+        
+        getPathIterator(tx : geom.transform.BaseTransform, flatness? : number) : PathIterator {
+            if (flatness !== undefined) {
+                return new FlatteningPathIterator(this.getPathIterator(tx), flatness);
+            } else {
+                if (tx == null) {
+                    return new CopyIterator(this);
+                } else {
+                    return new TxIterator(this, tx);
+                }
+            }
+        }
+
+        static copyOf(original : number[], newLength : number) : number[] {
+            let copy : number[] = [];
+            copy = original.slice(0);
+            return copy;
+        }
+
+        setTo(otherPath : geom.Path2D) : void {
+            this.numTypes  = otherPath.numTypes;
+            this.numCoords = otherPath.numCoords;
+            if (this.numTypes > this.pointTypes.length) {
+                this.pointTypes = [];
+            }
+            this.pointTypes = otherPath.pointTypes.slice(0);
+            if (this.numCoords > this.floatCoords.length) {
+                this.floatCoords = [];
+            }
+            this.floatCoords = otherPath.floatCoords.slice(0);
+            this.windingRule = otherPath.windingRule;
+            this.moveX = otherPath.moveX;
+            this.moveY = otherPath.moveY;
+            this.prevX = otherPath.prevX;
+            this.prevY = otherPath.prevY;
+            this.currX = otherPath.currX;
+            this.currY = otherPath.currY;
         }
 
         getNgNode(): scene.NGNode {
