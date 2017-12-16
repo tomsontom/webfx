95f64cb789e449ff98aa1d56795b46f14155648d
Gerrit Grunwald <han.solo@mac.com>
2017-12-16 07:26:54 +0100

    Added useful classes from geometry package

---
+++ geometry/Dimension2D.ts
@@ -0,0 +1,10 @@
+export class Dimension2D {
+    width  : number;
+    height : number;
+
+
+    constructor(width : number, height : number) {
+        this.width  = width;
+        this.height = height;
+    }
+}
--- geometry/Point2D.ts
+++ geometry/Point2D.ts
@@ -1,4 +1,8 @@
+import {Utils} from "../util/Utils";
+import {Point3D} from "./Point3D";
+
 export class Point2D {
+    public static readonly ZERO : Point2D = new Point2D(0, 0);
     x : number;
     y : number;
 
@@ -9,32 +13,56 @@ export class Point2D {
     }
 
 
-    setLocation(x : number, y : number) : void {
-        this.x = x;
-        this.y = y;
-    }
-
-    static distanceSq(x1 : number, y1 : number, x2 : number, y2 : number) : number {
-        x1 -= x2;
-        y1 -= y2;
-        return (x1 * x1 + y1 * y1);
-    }
-
-    static distance(x1 : number, y1 : number, x2 : number , y2 : number) : number {
-        x1 -= x2;
-        y1 -= y2;
-        return Math.sqrt(x1 * x1 + y1 * y1);
-    }
-
-    distanceSq(px : number, py : number) : number {
-        px -= this.x;
-        py -= this.y;
-        return (px * px + py * py);
-    }
-
     distance(px : number, py : number) : number {
         px -= this.x;
         py -= this.y;
         return Math.sqrt(px * px + py * py);
+    }
+
+    add(px : number, py : number) : Point2D {
+        return new Point2D(this.x + px, this.y + py);
+    }
+
+    subtract(px : number, py : number) : Point2D {
+        return new Point2D(this.x - px, this.y - py);
+    }
+
+    multiply(factor : number) : Point2D {
+        return new Point2D(this.x * factor, this.y * factor);
+    }
+
+    normalize() : Point2D {
+        var mag : number = this.magnitude();
+        if (mag == 0.0) {
+            return new Point2D(0, 0);
+        }
+        return new Point2D(this.x / mag, this.y / mag);
+    }
+
+    midpoint(px : number, py : number) : Point2D {
+        return new Point2D(px + (this.x - px) / 2.0, py + (this.y - py) / 2.0);
+    }
+
+    angle(px : number, py : number) : number {
+        var ax    = this.x;
+        var ay    = this.y;
+        var delta = (ax * px + ay * py) / Math.sqrt((ax * ax + ay * ay) * (px * px + py * py));
+
+        if (delta > 1.0)  { return 0.0; }
+        if (delta < -1.0) { return 180.0; }
+
+        return Utils.toDegrees(Math.acos(delta));
+    }
+
+    magnitude() : number {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    dotProduct(px : number, py : number) : number {
+        return this.x * px + this.y * py;
+    }
+
+    crossProduct(px : number, py : number) : Point3D {
+        return new Point3D(0, 0, this.x * py - this.y * px);
     }
 }
