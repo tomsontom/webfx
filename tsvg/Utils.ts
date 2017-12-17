dc6b6829730d9539a5187e42ae039e010f5dbc79
Tom Schindl <tom.schindl@bestsolution.at>
2017-12-18 00:38:44 +0100

    added length support
    refs #10

--- tsvg/Utils.ts
+++ tsvg/Utils.ts
@@ -1,8 +1,30 @@
+export type Length = number | string;
+
 export class ElementUtils {
     static clear( element : Element ) {
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
         return element;
+    }
+}
+
+export class LengthUtils {
+    static isValid(length: Length, percentage? : boolean) : boolean {
+        if( typeof length === "number" ) {
+            return true;
+        } 
+        var v = String(length);
+        if( percentage && v.endsWith("%") ) {
+            return true;
+        }
+        return v.endsWith("em")
+            || v.endsWith("ex")
+            || v.endsWith("px")
+            || v.endsWith("in")
+            || v.endsWith("cm")
+            || v.endsWith("mm")
+            || v.endsWith("pt")
+            || v.endsWith("pc");
     }
 }
